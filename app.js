9d58684c9cdb8517e9ab0318ef5e14870bdb2082
Kıvanc Aydogmus <kroytun77@gmail.com>
2021-08-31 04:00:43 +0300

    product avatar is arranged and it is provided by link

--- app.js
+++ app.js
@@ -8,55 +8,57 @@ const Product = require("./models/product")
 const app = express()
 //console.log(path.join(__dirname,"./public"))
 const port = 8080;
-const publicDirectory = path.join(__dirname,"./public")
+const publicDirectory = path.join(__dirname, "./public")
 app.use(express.static(publicDirectory))
 app.use(express.json()) // to accept json
-app.set("view engine","hbs")
-app.set("views",publicDirectory)
+app.set("view engine", "hbs")
+app.set("views", publicDirectory)
 
 
 
-app.get("/",(req,res) => {
-    res.render("index",{
+app.get("/", (req, res) => {
+    res.render("index", {
 
     })
 })
 
 const upload = multer({
-    dest = "avatars",
-    limits : {
-        fileSize : 1000000
+    //dest: "avatars", // we dont need anymore becasue we save avatar in server
+    limits: {
+        fileSize: 2000000
     },
-    fileFilter(req,file,cb) {
-        if(!file.originalname.endsWith(".jpg") || !file.originalname.endsWith(".jpeg") || !file.originalname.endsWith(".png")) {
-            cb(new Error("The extension of the file must be .jpg or .jpeg or .png"),false)
+    fileFilter(req, file, cb) {
+        if (!file.originalname.endsWith(".jpg") || file.originalname.endsWith(".jpeg") || file.originalname.endsWith(".png")) {
+            cb(new Error("The extension of file must be .jpg or .jpeg or .png"))
         }
-        cb(undefined,true);
+        cb(undefined, true)
     }
 })
 
-app.post("/product",async (req,res) => {
+app.post("/product", async (req, res) => {
     try {
         const product = await Product.preventDublicate(req.body);
-        res.status(201).send({product})
+        res.status(201).send({ product })
     } catch (error) {
-        res.status(400).send({error : "This name has been used already"})
+        res.status(400).send({ error: "This name has been used already" })
     }
 })
 
-app.post("/products/avatar:/id",upload.single("image"),async (req,res) => {
+app.post("/product/avatar/:id", upload.single("image"), async (req, res) => {
     const _id = req.params.id
     const product = await Product.findById(_id)
     if (!product) {
         throw new Error("there is no product to choose avatar")
     }
-//    product.image = 
-    res.status(200).send("succeed");
-},(err,req,res,next) => {
-    res.status(404).send({error : err.message})
+    product.image = req.file.buffer
+    await product.save()
+    res.send(req.file);
+
+}, (req, res, next) => {
+    res.status(404).send({ error: err.message })
 })
 
-app.get("/products",async (req,res) => {
+app.get("/products", async (req, res) => {
     try {
         const products = await Product.find({})
         if (!products) {
@@ -69,6 +71,19 @@ app.get("/products",async (req,res) => {
 })
 
 
-app.listen(port,() => {
+app.get("/products/:id/image",async (req,res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product || !product.image) {
+            throw new Error("There is no product or product image");
+        }
+        res.set("Content-Type","image/jpg") // to set content type as image
+        res.send(product.image)
+    } catch (error) {
+        
+    }
+})
+
+app.listen(port, () => {
     console.log("Server is available on port " + port)
 })
