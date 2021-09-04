f19ce944b08f8152cc5a6285d5a99ba1c976027c
Kıvanc Aydogmus <kroytun77@gmail.com>
2021-09-04 13:12:44 +0300

    the db connect url has been changed according to the docker container

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // sociality-test-db
 
-mongoose.connect("mongodb://127.0.0.1:27017/sociality-test-db",{
+mongoose.connect("mongodb://mongo/sociality-test-db",{
    autoCreate:true
 })
